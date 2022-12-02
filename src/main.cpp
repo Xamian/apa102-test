6a861ac7f5655f5a01fb93e7bf2791c2db7cd5ae
Max Stagsted <max@max-it.dk>
2022-12-02 16:08:13 +0100

    cleanup misleading comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,4 @@
 #include <Arduino.h>
-
-/// @file    Cylon.ino
-/// @brief   An animation that moves a single LED back and forth (Larson Scanner effect)
-/// @example Cylon.ino
-
 #include <FastLED.h>
 
 // How many leds in your strip?
@@ -42,14 +37,9 @@ void loop()
   // First slide the led in one direction
   for (int i = 0; i < NUM_LEDS; i++)
   {
-    // Set the i'th led to red
     leds[i] = CHSV(hue++, 255, 255);
-    // Show the leds
     FastLED.show();
-    // now that we've shown the leds, reset the i'th led to black
-    // leds[i] = CRGB::Black;
     fadeall();
-    // Wait a little bit before we loop around and do it again
     delay(10);
   }
   Serial.print("x");
@@ -57,14 +47,9 @@ void loop()
   // Now go in the other direction.
   for (int i = (NUM_LEDS)-1; i >= 0; i--)
   {
-    // Set the i'th led to red
     leds[i] = CHSV(hue++, 255, 255);
-    // Show the leds
     FastLED.show();
-    // now that we've shown the leds, reset the i'th led to black
-    // leds[i] = CRGB::Black;
     fadeall();
-    // Wait a little bit before we loop around and do it again
     delay(10);
   }
 }
